cd32f61c387b4461b9d2ac6390f9c1460ff2d42b
matt <matthewharrison@gmail.com>
2012-11-03 23:29:53 -0600

    added epub/jquery debug mode to comic books with MobiComicBook arg add_jquery (set to True for debug)

--- epublib/mobi.py
+++ epublib/mobi.py
@@ -10,7 +10,8 @@ class MobiComicBook(epub.EpubBook):
     def __init__(self, resolution='600x1024',
                  book_type='comic',
                  region_magnification='true',
-                 orientation_lock='none'):
+                 orientation_lock='none',
+                 add_jquery=False):
         epub.EpubBook.__init__(self)
         self.comic_meta = []
         self.add_comic_meta('fixed-layout', 'true')
@@ -28,6 +29,8 @@ class MobiComicBook(epub.EpubBook):
         self.page = None
         self.ordinal = 1
         self.target_id = 1
+
+        self.add_jquery = add_jquery
 
     def get_target_id(self):
         tid =  'zoom{0}'.format(self.target_id)
@@ -58,17 +61,33 @@ class MobiComicBook(epub.EpubBook):
 
 
 class Mag(object):
-    def __init__(self, txt, target_id_parent, target_id, top, left, height, width, img_top, img_left, img_src, ordinal=None, zoom_factor=1.5,
+    def __init__(self, txt, target_id_parent, target_id,
+                 left, top, width, height,
+                 lb_left, lb_top, lb_width, lb_height,
+                 zoomed_img_left, zoomed_img_top,
+                 img_src, ordinal=None, zoom_factor=1.5,
                  pre_data=None, post_data=None):
         self.txt = txt
         self.target_id_parent = target_id_parent
         self.target_id = target_id
+
+        # coordinates for click target
+
         self.top = top
         self.left = left
         self.height = height
         self.width = width
-        self.img_top = img_top
-        self.img_left = img_left
+
+        # lb_ refers to lightbox image
+        self.lb_top = lb_top
+        self.lb_left = lb_left
+        self.lb_width = lb_width
+        self.lb_height = lb_height
+
+        # zoomed_img_ referes to position on zoomed image relative to lightbox
+        self.zoomed_img_top = zoomed_img_top
+        self.zoomed_img_left = zoomed_img_left
+
         self.ordinal = ordinal
         self.zoom_factor = zoom_factor
         self.img_src = img_src
@@ -114,37 +133,32 @@ class Page(object):
         self.img = self.book.add_image(src_path, dest_path, id)
 
 
-    def add_zoom_image(self, zoom_factor, target_left, target_top,
-                       target_height, target_width,
+    def add_zoom_image(self, zoom_factor,
+                       target_left, target_top, target_width, target_height,
+                       lb_left, lb_top, lb_width, lb_height,
                        zoom_left, zoom_top,
                        src_path=None, dest_path=None, txt=None,
                        pre_data=None, post_data=None):
+        # post data is borked
         if src_path and dest_path:
             img = self.book.add_image(src_path, dest_path, None)
         else:
             img = self.img
         tid = self.book.get_target_id()
         ordinal = self.book.get_ordinal()
-        mag = Mag(txt, tid+self.PARENT_SUFFIX, tid, target_top, target_left,
-                    target_height, target_width, zoom_top, zoom_left,
-                    img.dest_path, ordinal,
-                    pre_data=pre_data, post_data=post_data)
+        mag = Mag(txt, tid+self.PARENT_SUFFIX, tid,
+            target_left, target_top, target_width, target_height,
+            lb_left, lb_top, lb_width, lb_height,
+            zoom_left, zoom_top,
+            img.dest_path, ordinal,
+            pre_data=pre_data, post_data=post_data)
         self.mags.append(mag)
-
-    def add_mag(self, target_id_parent, target_id, top, left, height, width, img_top, img_left):
-        ordinal = self.book.get_ordinal()
-        self.mags.append(Mag(None, target_id_parent, target_id, top, left, height, width, img_top, img_left, self.img.dest_path, ordinal))
-
-
-    def add_mag_text(self, text, target_id_parent, target_id, top, left, height_per, width_per, img_top, img_left):
-        ordinal = self.book.get_ordinal()
-        self.mags.append(Mag(txt, target_id_parent, target_id, top, left, height_per, width_per, img_top, img_left, self.img.dest_path, ordinal))
-
 
     def to_html(self):
         #temp = NewTextTemplate(PAGE_TEMPLATE)
         temp = self.book.loader.load('mobicomic2.html')
-        stream = temp.generate(page=self, body='', img_src=self.img.dest_path)
+        stream = temp.generate(page=self, body='', img_src=self.img.dest_path,
+                               add_jquery=self.book.add_jquery)
         html = stream.render('html', doctype= 'html-transitional')
         return html
 
@@ -376,31 +390,44 @@ opacity: .5;
         """
 
 def test():
-    book = MobiComicBook()
-    title = 'Test Comic34'
+    book = MobiComicBook(add_jquery=True)
+    title = 'Test Comic35'
     book.set_title(title)
     cov_image = 'data/little-nemo-19051015-l.jpeg'
     book.add_cover(cov_image, title=title)
     page = book.add_page()
     page.add_bg_image('data/little-nemo-19051015-l.jpeg', 'img1.jpeg')
     #page.add_mag('foo_id_parent', 'foo_id', 20, 20, 30, 60, 30, 30)
-    page.add_zoom_image(1.5, 10,10,50,50,15,12,pre_data='<div><p class="center">BEFORE</p></div>',
-                        post_data='<h2>AFTER</h2>')
-    page.add_zoom_image(1.5, 50,50,50,50,50,0,pre_data='<p>BEFORE2</p>',
-                         post_data='<h2>AFTER2</h2>')
+    page.add_zoom_image(1.5,
+                        0, 0, 50, 100./6,
+                        0, 50-100./6, 100, 20,
+                        0,0,
+                        pre_data='<div><p class="center">BEFORE</p></div>')
+    page.add_zoom_image(1.5,
+                        50, 0, 50, 100./6,
+                        0, 50-100./6, 100, 20,
+                        -50,0,
+                        pre_data='<div><p class="center">BEFORE</p></div>')
+    page.add_zoom_image(1.5,50,0,100./6,50,50,0,100,20,0,50,pre_data='<div><p class="center">BEFORE</p></div>')
+    # 3rd row
+    page.add_zoom_image(1.5,300./6,0,100./6,50,
+                        0,0,100,20,
+                        300./6,0,
+                        pre_data='<div><p class="center">BEFORE</p></div>')
+    #page.add_zoom_image(1.5, 50,50,50,50,50,0,pre_data='<p>BEFORE2</p>',
+    #                     post_data='<h2>AFTER2</h2>')
     # page.add_mag('foo_id_parent', 'foo_id', 50, 70, 80, 20, 70, 70)
     # page.add_mag('foo_id_parent2', 'foo_id2', 50, 70, 80, 20, 70, 70)
     h1 = page.add_html('1.html')
     book.add_spine_item(h1)
-    page = book.add_page()
-    page.add_bg_image('data/little-nemo-19051022-l.jpeg', 'img2.jpeg')
-    page.add_zoom_image(1.5, 0,0,50,50,0,0,pre_data='<p>BEFORE</p>',
-                        post_data='<h2>AFTER</h2>')
-    h2 = page.add_html('2.html')
-    book.add_spine_item(h2)
-    # page.add_mag('foo_id_parent', 'foo_id', 20, 20, 30, 60, 30, 30)
-    # page.add_mag('foo_id_parent2', 'foo_id2', 50, 70, 80, 20, 70, 70)
-    # page.add_html('2.html')
+
+    # page = book.add_page()
+    # page.add_bg_image('data/little-nemo-19051022-l.jpeg', 'img2.jpeg')
+    # page.add_zoom_image(1.5, 0,0,50,50,0,0,pre_data='<p>BEFORE</p>',
+    #                     post_data='<h2>AFTER</h2>')
+    # h2 = page.add_html('2.html')
+    # book.add_spine_item(h2)
+
     page.add_bg_image('data/little-nemo-19051105-l.jpeg', 'img3.jpeg')
     h3 = book.add_html('', '3.html', TEST_HTML)
     book.add_spine_item(h3)
